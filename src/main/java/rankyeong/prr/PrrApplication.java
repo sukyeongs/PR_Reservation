41577de6b16e1d1105abb829cd77905f57859c2e
sukyeong <ssk0967@naver.com>
2022-06-20 21:17:43 +0900

    Initial commit

---
+++ src/main/java/rankyeong/prr/PrrApplication.java
@@ -0,0 +1,13 @@
+package rankyeong.prr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rankyeong/prr/PrrApplicationTests.java
@@ -0,0 +1,13 @@
+package rankyeong.prr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
